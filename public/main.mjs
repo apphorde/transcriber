7a66194a0ac46f47907b83ae3d78ce21e64e7b6e
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-09-30 18:14:36 +0200

    fix: update template bindings

--- public/main.mjs
+++ public/main.mjs
@@ -1,5 +1,5 @@
 import vtt from "https://vtt.jsfn.run/index.mjs";
-import { computed, ref, watch, tpl, mount } from "@lithium/web";
+import { computed, ref, watch, tpl, mount, domReady } from "@lithium/web";
 import { useMicrophone } from "./useMicrophone.mjs";
 
 const template = tpl`
@@ -13,9 +13,9 @@ const template = tpl`
         <div class="flex items-center justify-between space-x-4 bg-darker rounded-full py-4 px-4">
           <div class="text-4xl font-bold text-white pl-4">{{ padLeft(minutes) }}:{{ padLeft(seconds) }}</div>
           <button
-            bind-class-name="'text-white font-bold py-4 px-4 rounded-full transition-colors duration-300 ' + (running.value ? 'bg-red-400 hover:bg-red-600 animate animate-pulse' : '')"
+            bind-class-name="'text-white font-bold py-4 px-4 rounded-full transition-colors duration-300 ' + (running ? 'bg-red-400 hover:bg-red-600 animate animate-pulse' : '')"
             on-click="onStart()"
-            bind-disabled="!supported.value"
+            bind-disabled="!supported"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -35,13 +35,13 @@ const template = tpl`
             </svg>
           </button>
         </div>
-        <template if="transcription.value || playbackUrl.value">
+        <template if="transcription || playbackUrl">
           <div class="space-y-6">
             <audio ref="player" bind-src="playbackUrl" class="w-full"></audio>
 
             <div class="flex items-center justify-between space-x-4">
               <button on-click="onPlayPause()" class="text-white py-3 px-8 rounded-full bg-darker text-center">
-                <template if="playing.value"
+                <template if="playing"
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="24"
@@ -52,7 +52,7 @@ const template = tpl`
                     <path d="M240-240v-480h480v480H240Z" />
                   </svg>
                 </template>
-                <template if="!playing.value">
+                <template if="!playing">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="24"
@@ -169,4 +169,4 @@ function setup() {
   };
 }
 
-mount("#app", { template, setup });
+domReady().then(() => mount("#app", { template, setup }));
